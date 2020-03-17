039914aaabfd779167358efeffcabb0f526165c7
Gustavo Souza <eugustavo@github.com>
2020-03-17 20:57:16 -0300

    Initial folder structure

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StatusBar, View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    </>
+  );
+};
+
+export default App;
